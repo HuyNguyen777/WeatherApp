168d9ddb86f944befef34d01f2f7949d600dcf57
HuyNguyen777 <huynguyen6498@gmail.com>
2025-04-17 22:09:47 +0700

    Initial commit

---
+++ src/app/Enviroment/EnviromentVariables.ts
@@ -0,0 +1,12 @@
+export const EnviromentVariables = {
+    production:false,
+    weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+    weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+
+
+    xRapidApiKeyName:'X-RapidAPI-Key',
+    xRapidApiKeyValue:'2cd754dac4msh44f495ed13b6289p16a89ejsndf4a433e8d06',
+
+    xRapidApiHostName:'x-rapidapi-host',
+    xRapidApiHostValue:'weather338.p.rapidapi.com'
+}
